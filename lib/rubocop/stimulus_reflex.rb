e43512806e568b7a12251da8281bd92642b97698
Marco Roth <marco.roth@intergga.ch>
2021-06-26 13:12:16 +0200

    initial commit

---
+++ lib/rubocop/stimulus_reflex.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "stimulus_reflex/version"
+
+module Rubocop
+  module StimulusReflex
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "rubocop/stimulus_reflex"
+
+require "minitest/autorun"
