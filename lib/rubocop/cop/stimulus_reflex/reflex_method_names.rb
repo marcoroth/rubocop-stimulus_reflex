8441c521f79a9dabe45000d380e4d030a0151c94
Marco Roth <marco.roth@intergga.ch>
2021-06-26 14:21:59 +0200

    implement cop `StimulusReflex/ReflexMethodNames`

--- lib/rubocop/cop/stimulus_reflex/reflex_method_names.rb
+++ lib/rubocop/cop/stimulus_reflex/reflex_method_names.rb
@@ -4,7 +4,18 @@ module RuboCop
   module Cop
     module StimulusReflex
       class ReflexMethodNames < Base
-        # TODO
+        NOT_RECOMMENED_METHOD_NAMES = [:send].freeze
+
+        def on_def(definition)
+          name = definition.method_name
+
+          return unless NOT_RECOMMENED_METHOD_NAMES.include?(name)
+
+          add_offense(
+            definition,
+            message: "It's not recommended to use `#{name}` as a reflex method name."
+          )
+        end
       end
     end
   end
